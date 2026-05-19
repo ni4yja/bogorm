cbd79b373a0764e04ff2d4b9608b695b2013682c
Liuba Kuibida <k2.liuba@gmail.com>
2026-05-19 18:07:33 +0200

    fix code to work

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -111,6 +111,7 @@ REST_FRAMEWORK = {
     "DEFAULT_VERSION": "v1",
     "ALLOWED_VERSIONS": ["v1"],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
 }
 
