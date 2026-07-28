1bb7d2568dfecf7e484cab7fe566704b21f072f5
Liuba Kuibida <k2.liuba@gmail.com>
2026-07-28 11:11:23 +0200

    feat(users): register CustomUser in Django admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ["email", "username", "is_staff", "is_active"]
+    ordering = ["email"]
