0b700f9511368ce7b94066f90bc802586a4f64d1
Liuba Kuibida <k2.liuba@gmail.com>
2026-07-31 11:01:08 +0200

    fix(users): show email field in admin add-user form

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -9,3 +9,13 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ["email", "username", "is_staff", "is_active"]
     ordering = ["email"]
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "username", "password1", "password2"),
+            },
+        ),
+    )
