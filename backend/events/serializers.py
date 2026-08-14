fa9ff853d03135ca600f5257be2e16e28b48fee7
Liuba Kuibida <k2.liuba@gmail.com>
2026-08-14 15:03:12 +0200

    feat: add lat/lng to nested place in EventListSerializer, update tests

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -6,9 +6,18 @@ from .models import Event
 
 
 class PlaceMinimalSerializer(serializers.ModelSerializer):
+    lat = serializers.SerializerMethodField()
+    lng = serializers.SerializerMethodField()
+
+    def get_lat(self, obj):
+        return obj.location.y
+
+    def get_lng(self, obj):
+        return obj.location.x
+
     class Meta:
         model = Place
-        fields = ["id", "title"]
+        fields = ["id", "title", "lat", "lng"]
 
 
 class EventSerializer(serializers.ModelSerializer):
