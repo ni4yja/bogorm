650149a7764f5471dfc1a1a77394c2cce97c58ec
Liuba Kuibida <k2.liuba@gmail.com>
2026-08-17 20:28:36 +0200

    refactor(places): extract CoordinatesMixin for lat/lng fields

--- backend/places/serializers.py
+++ backend/places/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Place
 
 
-class PlaceSerializer(serializers.ModelSerializer):
+class CoordinatesMixin(serializers.Serializer):
     lat = serializers.SerializerMethodField()
     lng = serializers.SerializerMethodField()
 
@@ -13,6 +13,8 @@ class PlaceSerializer(serializers.ModelSerializer):
     def get_lng(self, obj):
         return obj.location.x
 
+
+class PlaceSerializer(CoordinatesMixin, serializers.ModelSerializer):
     class Meta:
         model = Place
         fields = [
@@ -28,32 +30,15 @@ class PlaceSerializer(serializers.ModelSerializer):
         ]
 
 
-class PlaceMapSerializer(serializers.ModelSerializer):
-    lat = serializers.SerializerMethodField()
-    lng = serializers.SerializerMethodField()
+class PlaceMapSerializer(CoordinatesMixin, serializers.ModelSerializer):
     event_count = serializers.IntegerField(read_only=True)
-
-    def get_lat(self, obj):
-        return obj.location.y
-
-    def get_lng(self, obj):
-        return obj.location.x
 
     class Meta:
         model = Place
         fields = ["id", "title", "lat", "lng", "category", "event_count"]
 
 
-class PlaceMinimalSerializer(serializers.ModelSerializer):
-    lat = serializers.SerializerMethodField()
-    lng = serializers.SerializerMethodField()
-
-    def get_lat(self, obj):
-        return obj.location.y
-
-    def get_lng(self, obj):
-        return obj.location.x
-
+class PlaceMinimalSerializer(CoordinatesMixin, serializers.ModelSerializer):
     class Meta:
         model = Place
         fields = ["id", "title", "lat", "lng"]
