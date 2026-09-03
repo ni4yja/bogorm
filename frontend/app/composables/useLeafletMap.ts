8a02dd1f9ff5422024aeb0ab2e896327da6bb373
Liuba Kuibida <k2.liuba@gmail.com>
2026-09-03 16:59:29 +0200

    fix: add Carto API key to remove basemap watermark

--- frontend/app/composables/useLeafletMap.ts
+++ frontend/app/composables/useLeafletMap.ts
@@ -1,13 +1,17 @@
 export async function useLeafletMap(elementId: string) {
   const L = await import('leaflet')
+  const config = useRuntimeConfig()
 
   const map = L.map(elementId, { zoomControl: false }).setView([52.23, 21.01], 13)
 
   L.control.zoom({ position: 'topright' }).addTo(map)
 
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-    attribution: '© OpenStreetMap contributors © CARTO',
-  }).addTo(map)
+  L.tileLayer(
+    `https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png?key=${config.public.cartoApiKey}`,
+    {
+      attribution: '© OpenStreetMap contributors © CARTO',
+    },
+  ).addTo(map)
 
   const markersLayer = L.layerGroup().addTo(map)
 
